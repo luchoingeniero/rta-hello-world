e84a3f8488dd75a842d3c262a5cb5ba8e88b9d88
Luis Galeano <ing.lgaleano@gmail.com>
2020-03-12 10:45:34 -0500

    se agrego el server con los eventos de mensajeria

---
+++ server/server.js
@@ -0,0 +1,30 @@
+var http = require('http').Server();
+var io = require('socket.io')(http);
+var HOST = 'localhost';
+var PORT_SOCKET=3000;
+
+var events = {
+  newConnection: 'connection', 
+  offConnnection: 'disconnect', 
+  newMessage: 'new_message',
+  emitMessage: 'emit_message'
+} 
+
+
+io.on(events.newConnection, function(socket) {
+
+  socket.on(events.newMessage, function(message) {
+
+    io.sockets.emit( events.emitMessage , message);
+  
+  });
+
+  socket.on(events.offConnnection, () => {
+        console.log('user disconnected');
+  });
+
+});
+
+http.listen(PORT_SOCKET, function(){
+  console.log('Esperando Clientes on '+HOST+':'+PORT_SOCKET);
+});
